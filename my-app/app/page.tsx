3a0faecebc603ac863a495136dfe8c625cfa7a98
Andlie94 <andlie02174@stud.noroff.no>
2025-08-25 14:14:21 +0200

    added next.js
    added next.js

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <body className="bg-white">
+        <h1>Hello World</h1>
+      </body>
+    </div>
+  );
+}
