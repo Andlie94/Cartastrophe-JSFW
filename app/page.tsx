d7b870684370c7e4a52913a1fb523e08f2711194
Andlie94 <andlie02174@stud.noroff.no>
2025-08-27 22:50:29 +0200

    Instaled tailwind config file. it dont seems to work. have to try it later tomorrow

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-[#7A7AD2]">Hello world!</h1>
       <h2>Test for h2</h2>
       <p>This is a test to see the paragraph.</p>
       <Button onClick={handleClick}>Trykk meg</Button>
