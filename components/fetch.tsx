7a8fcf0d70c41987a93fcced9361c5e0bf3b43a9
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-09-02 12:52:46 +0200

    fetched product id to individual page

--- components/fetch.tsx
+++ components/fetch.tsx
@@ -13,3 +13,14 @@ export const getProducts = async () => {
     return [];
   }
 };
+
+export const getProductById = async (id: string) => {
+  try {
+    const res = await fetch(`https://v2.api.noroff.dev/online-shop/${id}`, { cache: "no-store" });
+    if (!res.ok) throw new Error("Failed to fetch product");
+    const json = await res.json();
+    return json.data;
+  } catch {
+    return null;
+  }
+};
